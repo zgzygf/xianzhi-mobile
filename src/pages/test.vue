c959d90afec32611bcf934d552cea043299640d9
zgzygf <2481162091@qq.com>
2018-01-29 11:16:25 +0800

    搭建鲜致移动端框架

---
+++ src/pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+   <div>
+    <mt-tabbar >
+    <mt-tab-item id="tab1">
+        <img slot="icon" >
+        tab1
+    </mt-tab-item>
+    <mt-tab-item id="tab2">
+        <img slot="icon" >
+        tab2
+    </mt-tab-item>
+    <mt-tab-item id="tab3">
+        <img slot="icon" >
+        tab3
+    </mt-tab-item>
+    <mt-tab-item id="tab4">
+        <img slot="icon" >
+        tab4
+    </mt-tab-item>
+    </mt-tabbar>
+   </div>
+</template>
+<script>
+export default {
+ name:"test"
+}
+</script>
